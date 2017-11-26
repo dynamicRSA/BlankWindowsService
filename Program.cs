0c8b0ada49c92583f5c3bac2e2460bd3a0d76c65
Sven <svenprinsloo@gmail.com>
2017-11-26 20:54:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+using System.ServiceProcess;
+using log4net;
+
+namespace BlankService
+{
+    static class Program
+    {
+       // public static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static int intervals = Convert.ToInt32(GetConfigValue("intervals"));
+
+        
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new BlankService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+        private static string GetConfigValue(string key)
+        {
+            string value = string.Empty;
+            try
+            {
+                value  = ConfigurationManager.AppSettings[key];
+
+                //if (value == null) throw new ConfigurationException(string.Format("Could not find key {0} in the configuration file.", key));
+                log.Info("============ BlankService stopped ============");
+
+            }
+            catch (Exception)
+            {
+                //throw new ConfigurationException(string.Format("Could not find key {0} in the configuration file.", key));
+                log.Info("============ BlankService stopped ============");
+            }
+            return value;
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,56 @@
+﻿
+using System.ServiceProcess;
+using System.Timers;
+
+
+namespace BlankService
+{
+    public partial class BlankService : ServiceBase
+    {
+        Timer tmrDelay;
+        int count;
+        System.DateTime scheduledTime = System.DateTime.MinValue;
+        
+        
+
+
+        public BlankService()
+        {
+            InitializeComponent();
+
+            if (!System.Diagnostics.EventLog.SourceExists("MyLogSrc"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource("MyLogSrc", "MyLog");
+            }
+
+            //set our event log to system created log
+            eventLog1.Source = "MyLogSrc";
+            eventLog1.Log = "MyLog";
+
+            tmrDelay = new System.Timers.Timer(Program.intervals); 
+            tmrDelay.Elapsed += new System.Timers.ElapsedEventHandler(tmrDelay_Elapsed);
+
+        }
+
+        void tmrDelay_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            //logic for service - on timer
+            //configure intervals of timer in config
+            Program.log.Info("============ BlankService Log info on interval ============");
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Program.log.Info("============ BlankService started ============");
+            eventLog1.WriteEntry("BlankService started");
+            tmrDelay.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            Program.log.Info("============ BlankService stopped ============");
+            eventLog1.WriteEntry("BlankService stopped");
+            tmrDelay.Enabled = false;
+        }
+    }
+}
